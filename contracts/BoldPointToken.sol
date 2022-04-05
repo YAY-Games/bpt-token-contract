ce075f5d80a63f106ecca8bbe4482f44784caac5
Алексей Жданов <zhdanoff.alexey@gmail.com>
2022-04-05 12:04:15 +0300

    fix style

--- contracts/BoldPointToken.sol
+++ contracts/BoldPointToken.sol
@@ -13,7 +13,7 @@ pragma solidity 0.8.13;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";                                                                                                         
+import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
